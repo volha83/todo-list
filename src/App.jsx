ae9cac7c72d377d33055cd3e0ed84c20b8eb0e22
Volha Padlipskaya <o.podlipskaya@Mac.lan>
2025-07-27 23:19:45 -0400

    added props and state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css';
 import TodoList from './TodoList';
 import TodoForm from './TodoForm';
+import TodoListItem from './TodoListItem';
+import React, { useState } from 'react';
 
 function App() {
+  const [newTodo, setNewTodo] = useState('my example text');
   return (
     <div>
       <h1>Todo List</h1>
       <TodoForm />
+      <p>{newTodo}</p>
       <TodoList />
     </div>
   );
